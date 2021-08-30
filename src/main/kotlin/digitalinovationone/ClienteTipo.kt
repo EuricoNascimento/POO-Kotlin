020266ddfc679e36ae36c30b14a813561f8371f1
Eurico Neto <euriconascimentofn@gmail.com>
2021-08-30 14:14:37 -0300

    Atualização de ensino

---
+++ src/main/kotlin/digitalinovationone/ClienteTipo.kt
@@ -0,0 +1,6 @@
+package digitalinovationone
+
+enum class ClienteTipo(descricao: String) {
+    PF("Pessoa Fisica"),
+    PJ("Pessoa Juridica");
+}
--- src/main/kotlin/digitalinovationone/Conta.kt
+++ src/main/kotlin/digitalinovationone/Conta.kt
@@ -1,4 +1,7 @@
 package digitalinovationone
 
-class Conta {
+import java.math.BigDecimal
+
+class Conta (val agencia: String, val numero: String, val saldo: BigDecimal) {
+
 }
